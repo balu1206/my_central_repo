c393e411c3156094b7f86ae63e48aae87b06c66b
bhaskar <balu.svb000@gmail.com>
2021-12-15 16:31:05 +0530

    created a function

--- python.py
+++ python.py
@@ -1,7 +1,11 @@
 number=[1,2,3,4,5]
-for num in number:
-	print(x)
+def light_weight_tags():
+	
+	for num in number:
+	  print(x) 
 if num == 5:
 print ("foundthe number")
 else:
 print("did not found the number")  
+
+light_weight_tags()
